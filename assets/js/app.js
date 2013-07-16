b50a43aeed0e60f2c82a6465384bf5b4cadf1d4b
Joe Burton <joe.burton@bauerservices.co.uk>
2013-07-16 13:57:35 +0100

    added sql file

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,19 +8,20 @@ $(function () {
 	use this approach for development and static content
 	*/
 	
-	app.items = new app.ItemCollection([
-		{id: 1, firstname: "Albert", surname: "Einstein"},
-		{id: 2, firstname: "William", surname: "Drake"},
-		{id: 3, firstname: "Michael", surname: "Jackson"},
-		{id: 4, firstname: "Walt ", surname: "Disney"},
-		{id: 5, firstname: "John", surname: "Lennon"}
-	]);
+	// app.items = new app.ItemCollection([
+	// 	{id: 1, firstname: "Albert", surname: "Einstein"},
+	// 	{id: 2, firstname: "William", surname: "Drake"},
+	// 	{id: 3, firstname: "Michael", surname: "Jackson"},
+	// 	{id: 4, firstname: "Walt ", surname: "Disney"},
+	// 	{id: 5, firstname: "John", surname: "Lennon"}
+	// ]);
 
-	new app.ItemListView({collection: app.items}).render();	
+	// new app.ItemListView({collection: app.items}).render();	
 
 
 	/*
 	// call to MySQL databse to get resutls
+	*/
 	
 	app.items = new app.ItemCollection();
 	app.items.fetch({
@@ -32,6 +33,6 @@ $(function () {
 		}
 
 	});
-	*/
+	
 
 });
