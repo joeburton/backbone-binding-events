3b41685d8a1c2414db99cc2bc2d4fa67ef5e4c19
Joe Burton <joe.burton@bauerservices.co.uk>
2013-07-25 10:38:21 +0100

    latest updstes to events and app framework

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,40 @@
 var app = app || {};
 
-
-$(function () {
-	'use strict';
+;(function($, window){
+    
+ 	'use strict';
 
 	/*
 	use this approach for development and static content
 	*/
 	
-	// app.items = new app.ItemCollection([
-	// 	{id: 1, firstname: "Albert", surname: "Einstein"},
-	// 	{id: 2, firstname: "William", surname: "Drake"},
-	// 	{id: 3, firstname: "Michael", surname: "Jackson"},
-	// 	{id: 4, firstname: "Walt ", surname: "Disney"},
-	// 	{id: 5, firstname: "John", surname: "Lennon"}
-	// ]);
+	app.items = new app.ItemCollection([
+		{id: 1, firstname: "Albert", surname: "Einstein"},
+		{id: 2, firstname: "William", surname: "Drake"},
+		{id: 3, firstname: "Michael", surname: "Jackson"},
+		{id: 4, firstname: "Walt ", surname: "Disney"},
+		{id: 5, firstname: "John", surname: "Lennon"}
+	]);
 
-	// new app.ItemListView({collection: app.items}).render();	
+	new app.ItemListView({collection: app.items}).render();	
 
 
 	/*
-	// call to MySQL databse to get resutls
+	call to MySQL databse to get resutls
 	*/
-	
-	app.items = new app.ItemCollection();
-	app.items.fetch({
 
-		success:function () {
+	// app.items = new app.ItemCollection();
+	// app.items.fetch({
+
+	// 	success:function () {
 			
-			new app.ItemListView({collection: app.items}).render();
+	// 		new app.ItemListView({collection: app.items}).render();
 
-		}
+	// 	}
 
-	});
-	
+	// });
 
-});
+	// console.log( "version: " + $.fn.jquery + "<br>" );
+	// console.log( "version: " + jQuery.fn.jquery + "<br>" );
+
+})(jQuery, this);
